f5163feeee5c4a705cc562efed48589b6760b9bc
vinc3661 <vincemuthee387@gmail.com>
2026-09-23 23:37:37 +0300

    user credentials

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -2,8 +2,10 @@ import { createUserWithEmailAndPassword } from "firebase/auth";
 import {auth} from "./firebase";
 export async function registerUser(email:string,password:string):Promise<void>{
     try{
-        await createUserWithEmailAndPassword(auth,email,password);
-    }catch(error){
+    const credentials =  await createUserWithEmailAndPassword(auth,email,password);
+ console.log('user regitered');
+ console.log('user id');   
+}catch(error){
         console.error('failed to register user:',error);
     }
 };
